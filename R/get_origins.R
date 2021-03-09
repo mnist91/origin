50bf59b9cad76fca76ddae06cfbd0156041f0961
mnist_91 <matthias.nistler@statworx.com>
2021-03-09 08:27:32 +0100

    SETUP: originize core functionality revised - skip ci

---
+++ R/get_origins.R
@@ -0,0 +1,95 @@
+#' Add Package Name to Function Calls
+#'
+#' @param pkg a package name
+#' @param file a path to a script
+#' @param overwrite a boolean, if TRUE the file will be saved and overwritten. If FALSE the file is returned.
+#' @param ignoreComments a boolean, if TRUE lines starting with # are ignored
+#' @param verbose a boolean
+#' @param functions a vector with function names
+#'
+#' @noRd
+#' @return
+
+get_origins <- function(pkg,
+                        script,
+                        file = NULL,
+                        overwrite = FALSE,
+                        ignoreComments = TRUE,
+                        verbose = FALSE,
+                        functions = NULL) {
+  
+  
+  # get all exported functions from a package --------------------------------
+  if (is.null(functions)) {
+    functions <- getFunctions(pkg)
+  }
+  
+  l <- checkFunctions(script = script,
+                      functions = functions,
+                      pkg = pkg,
+                      verbose = verbose)
+  
+  # make function-Output available
+  matches <- l$matches
+  lineMatches <- l$lineMatches
+  special_matches <- l$special_matches
+  functionsInScript <- l$functionsInScript
+  special_functions <- l$special_functions
+  if (!any(matches)) {
+    return(list())
+  }
+  
+  
+  
+  
+  funs_comb <- paste0(functionsInScript, collapse = "|")
+  funs_prep <-  gsub("\\.", "\\\\.", x = funs_comb)
+  pattern_regex <- paste0("(?<=[, \\(!\\[]|^)(", funs_prep, ") *\\(")
+  
+  regular_calls <- get_matches(script[lineMatches],
+                               line = which(lineMatches),
+                               regex = pattern_regex,
+                               perl = TRUE,
+                               filter_nomatches = FALSE)
+  # In functional programming, the function's call is used without
+  # brackets but as an object. To differentiate between such a
+  # function and a variable, an explicit parameter is needed:
+  # e.g. *apply "FUN = "
+  # e.g. purr ".f = "
+  # regular expression, to identify usage with *apply/purrr
+  funArguments <- c("FUN", "\\.f")
+  assignWithSpaces <- c("=", " = ", "= ", " =")
+  leadingPatterns <- paste(apply(expand.grid(funArguments, assignWithSpaces),
+                                 MARGIN = 1,
+                                 FUN = paste0,
+                                 collapse = ""),
+                           collapse = "|")
+  functionalPatternRegex <-
+    paste0("(?<=", leadingPatterns, ")(",
+           funs_prep, ")(?=[ \\,)]|$)")
+  functional_calls <- get_matches(script[lineMatches],
+                                  line = which(lineMatches),
+                                  regex = functionalPatternRegex,
+                                  perl = TRUE,
+                                  fixed = FALSE,
+                                  filter_nomatches = FALSE)
+  
+  matches <- Map(comb_matches,
+                 functional_calls$matches,
+                 regular_calls$matches)
+  
+  has_match <- lapply(matches, length) != 0
+  
+  n_matches <- sum(has_match)
+  orig_list <- list(line = regular_calls$line[has_match],
+                    string = regular_calls$string[has_match],
+                    matches = matches[has_match],
+                    # log_length = Map(comb_matches,
+                    #                  functional_calls$match_length,
+                    #                  regular_calls$match_length)[has_match],
+                    log_length = lapply(matches[has_match], function(x) rep(0, length(x))),
+                    pkg = rep(paste0(pkg, "::"), n_matches),
+                    type = rep("insert", n_matches))
+  
+  return(orig_list)
+}
--- R/originize.R
+++ R/originize.R
@@ -1,137 +1,114 @@
-#' Add Package Name to Function Calls
+#' Add Explicit Package Names to its Functions
 #'
-#' @param pkg a package name
 #' @param file a path to a script
+#' @param pkgs a vector with package names
 #' @param overwrite a boolean, if TRUE the file will be saved and overwritten. If FALSE the file is returned.
 #' @param ignoreComments a boolean, if TRUE lines starting with # are ignored
+#' @param excludeBasePackages a boolean, if TRUE base R functions are excluded
 #' @param verbose a boolean
-#' @param functions a vector with function names
 #'
-#' @noRd
 #' @return
-
-originize <- function(pkg,
-                      file = NULL,
+#' @export
+#'
+originize <- function(script,
+                      file,
+                      functions,
+                      pkgs = .packages(),
                       overwrite = FALSE,
                       ignoreComments = TRUE,
+                      excludeBasePackages = TRUE,
                       verbose = FALSE,
-                      functions = NULL) {
+                      html = TRUE) {
   
   
-  if (!file.exists(file)) {
-    stop("No file in this path\n", file)
-    # load file
-  } else {
-    script <- readLines(file)
-  }
-  
-  # save prior script for comparison
-  if (verbose) {
-    script_prior <- script
-  }
-  
-  if (is.null(functions)) {
-    # get all exported functions from a package --------------------------------
-    functions <- getFunctions(pkg)
-  }
-  
+  # get relevant function information
   l <- checkFunctions(script = script,
-                      functions = functions,
-                      pkg = pkg,
-                      verbose = verbose)
+                      functions = unlist(functions),
+                      verbose = verbose,
+                      ignoreComments = ignoreComments)
   
-  # make function-Output available
-  matches <- l$matches
-  lineMatches <- l$lineMatches
-  special_matches <- l$special_matches
-  functionsInScript <- l$functionsInScript
-  special_functions <- l$special_functions
-  if (!any(matches)) {
-    return(NULL)
-  }
+  # iterate over all functions and find position where package:: is necessary
+  replacement_list <- 
+    Map(f = function(pkg, funs) {
+      get_origins(pkg            = pkg,
+                  script         = script,
+                  functions      = funs,
+                  file           = file,
+                  overwrite      = overwrite,
+                  ignoreComments = ignoreComments,
+                  verbose        = verbose)
+    },
+    pkgs,
+    functions
+    )
+  
+  # combine results for all packages
+  combined <- Reduce(f = function(...) {
+    Map(f = c,
+        ...)
+  },
+  x = replacement_list)
+  
+  # insert package:: to the relevant positions
+  fixed_lines_list <- lapply(
+    X = sort(unique(combined$line)), 
+    FUN = prep_line_originize,
+    combined = combined)
+  
+  # combine all lines
+  fixed_lines_dat <- Reduce(rbind, fixed_lines_list)
   
   
-  # regular expression, to prefix package name to functions
-  replacementRegex <- paste0("\\1", pkg, "::\\2\\3")
-  
-  # regular expression, to identify usage with *apply/purrr
-  funArguments <- c("FUN", "\\.f")
-  assignWithSpaces <- c("=", " = ", "= ", " =")
-  leadingPatterns <- paste(apply(expand.grid(funArguments, assignWithSpaces),
-                                 MARGIN = 1,
-                                 FUN = paste0,
-                                 collapse = ""),
-                           collapse = "|")
-  
-  script[lineMatches] <-
-    # iterate over all scripts' rows
-    purrr::map_chr(
-      .x = script[lineMatches],
-      .f = function(LINE) {
-        # iterate over all functions
-        # using reduce to not override prevoius changed functions
-        purrr::reduce(
-          .x = functionsInScript,
-          .f = function(STRING, FUN) {
-            
-            # gsub only changes the first occurrence, hence a while loop
-            # potentially changes this code part
-            
-            # function name must be after a comma, space or an open bracket
-            # or at the beginning of a row
-            # in addition it must be followed by an open round bracket
-            FUN <- gsub("\\.", "\\\\.", x = FUN)
-            patternRegex <- paste0("(.*)((?<=[, \\(]|^)", FUN, " *\\()(.*)")
-            
-            # In functional programming, the function's call is used without
-            # brackets but as an object. To differentiate between such a
-            # function and a variable, an explicit parameter is needed:
-            # e.g. *apply "FUN = "
-            # e.g. purr ".f = "
-            functionalPatternRegex <-
-              paste0("(.*)((?<=", leadingPatterns, ")",
-                     FUN, "(?=[ \\,)]|$))(.*)")
-            
-            repeat{
-              STRING_PRIOR <- STRING
-              STRING <- gsub(x = STRING,
-                             pattern = patternRegex,
-                             replacement = replacementRegex,
-                             perl = TRUE)
-              STRING <- gsub(x = STRING,
-                             pattern = functionalPatternRegex,
-                             replacement = replacementRegex,
-                             perl = TRUE)
-              if (STRING_PRIOR == STRING) {
-                break
-              }
-            }
-            
-            return(STRING)
-          },
-          .init = LINE)
-      })
-  
-  if (verbose) {
+  # if no logging is desired, skpi all relevant steps
+  if (!verbose) {
+    script[fixed_lines_dat$line] <- fixed_lines_dat$string
     
-    # which packet was analyzed
-    cat(crayon::red(pkg, paste(rep("-", 100 - nchar(pkg)),
-                               collapse = "")), "\n")
+    return(list(to_write = list(file = file, script = script),
+                marker_data = data.frame()))
     
-    verbolize(script_prior = script_prior,
-              script = script,
-              lineMatches = lineMatches,
-              functions = functions,
-              functionsInScript = functionsInScript,
-              special_functions = special_functions,
-              special_matches = special_matches)
-  }
-  
-  
-  if (overwrite) {
-    writeLines(script, con = file)
-    return(NULL)
   } else {
-    return(script)
+    
+    # get positions of potential missined (special) functions
+    potential_missings <- prep_verbose(script_prior = script,
+                                       lineMatches = l$lineMatches,
+                                       functions = unlist(functions),
+                                       functionsInScript = l$functionsInScript,
+                                       special_functions = l$special_functions,
+                                       special_matches = l$special_matches)
+    
+    # combine positions of potential missings
+    logging_comb <-  Reduce(
+      f = function(...) {
+        Map(f = c,
+            ...)
+      }, 
+      x = c(replacement_list,
+            Filter(f = Negate(is.null), 
+                   x = potential_missings)))
+    
+    
+    # prepare strings and insert color highlighting where needed
+    fixed_lines_list <- lapply(
+      X = sort(unique(logging_comb$line)), 
+      FUN = prep_line_logging,
+      logging_comb = logging_comb,
+      html = html)
+    
+    # combine all lines
+    logging_data <- Reduce(rbind, fixed_lines_list)
+    
+    if (html) {
+      # add further attributes for markers output
+      logging_data$type <- "info"
+      logging_data$file <- file
+      logging_data$column <- 1
+      attr(logging_data$message, which = "class") <- c("html", "character")
+    }
+    script[fixed_lines_dat$line] <- fixed_lines_dat$string
+    
+    
+    return(list(to_write = list(file = file, script = script),
+                logging_data = logging_data))
   }
+  
 }
