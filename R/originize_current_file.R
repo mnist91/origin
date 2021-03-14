7f4c4b130c8207c45939fef83eeed6f0dc98f8ab
mnist_91 <matthias.nistler@statworx.com>
2021-03-14 10:09:05 +0100

    CHORE: fix lints and add .lintr file - skip ci

--- R/originize_current_file.R
+++ R/originize_current_file.R
@@ -7,9 +7,25 @@
 #' @param pkgs a vector with package names
 #' @param overwrite a boolean, if TRUE the file will be saved and overwritten.
 #'    If FALSE the file is returned.
-#' @param ignore_comments a boolean, if TRUE lines starting with # are ignored
-#' @param excludeBasePackages a boolean, if TRUE base R functions are excluded
-#' @param verbose a boolean
+#' @param ask_before_applying_changes a boolean. If TRUE, the user has to approve
+#'   changes made by origin prior to applying them
+#' @param ignore_comments a boolean, if TRUE, lines starting with # are ignored
+#' @param check_conflicts a boolean. If TRUE, possible namespace conflicts are
+#'   checked and, if there are any, user input is required. See also
+#' @param check_base_conflicts a boolean. If TRUE; base R functions are also
+#'   included in checking for conflicts
+#' @param add_base_packages a boolean. If TRUE, base R functions are handled
+#'   like all other packages and added via `::`
+#' @param excluded_functions a list. Either an unnamed list of function names
+#'   as strings. Then these functions are excluded from all packages and never 
+#'   considered in origin. Or a named list with character vectors, Then 
+#'   the name of the list element refers to a package and the given functions 
+#'   are only excluded from this package. A very explicit way to handle
+#'   namespace conflicts
+#' @param verbose a boolean. If TRUE, origin provides feedback about its steps.
+#' @param use_markers a boolean. If TRUE, the markers tab inn RStudio is used
+#'   to track changes and show issues. FALSE prints the same informaiton in
+#'   the console.
 #'
 #' @return
 #' @export
