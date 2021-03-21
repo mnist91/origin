3ba2a266c9e104f4dae3e81a366f840759ca0df6
mnist_91 <matthias.nistler@statworx.com>
2021-03-21 13:50:15 +0100

    CI: print debugging

--- tests/testthat/test_origin_file.R
+++ tests/testthat/test_origin_file.R
@@ -1,27 +1,19 @@
-# Push dummy R script to a temp directory --------------------------------------
-dir <- tempdir()
-test_file_path <- file.path(dir, "testfile.R")
-target_file_path <- file.path(dir, "targetfile.R")
-
-# works only when package is build
-datapath <- system.file("testdata", package = "origin")
-
-test_text <- read.csv2(file = file.path(datapath, "testscript.csv"),
-                       na.strings = "NA",
-                       stringsAsFactors = FALSE)
-
-print(paste("datapath:",datapath))
-print(paste("target_file_path:",target_file_path))
-print(paste("test_file_path:",test_file_path))
-print(paste("nrow:",nrow(test_text)))
-print(paste("names:",names(test_text)))
-print(paste("test_text:",test_text[1, ]))
-writeLines(test_text$TARGET, con = target_file_path)
 
 
 
 # Unit tests
 testthat::test_that("origin file", {
+  # Push dummy R script to a temp directory --------------------------------------
+  dir <- tempdir()
+  test_file_path <- file.path(dir, "testfile.R")
+  target_file_path <- file.path(dir, "targetfile.R")
+
+  # works only when package is build
+  datapath <- system.file("testdata", package = "origin")
+
+  test_text <- read.csv2(file = file.path(datapath, "testscript.csv"),
+                         na.strings = "NA",
+                         stringsAsFactors = FALSE)
   print("in testthat")
   print(paste("datapath:",datapath))
   print(paste("target_file_path:",target_file_path))
@@ -29,6 +21,7 @@ testthat::test_that("origin file", {
   print(paste("nrow:",nrow(test_text)))
   print(paste("names:",names(test_text)))
   print(paste("test_text:",test_text[1, ]))
+  writeLines(test_text$TARGET, con = target_file_path)
   writeLines(test_text$TESTSKRIPT, con = test_file_path)
   script <- readLines(test_file_path)
 
