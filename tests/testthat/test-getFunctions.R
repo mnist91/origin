acfeff9b121493751348cc8538a5412471b7565e
mnist_91 <matthias.nistler@statworx.com>
2021-03-27 08:15:00 +0100

    TESTS: name according to function name

--- tests/testthat/test-getFunctions.R
+++
@@ -1,5 +0,0 @@
-testthat::test_that("export type is right", {
-
-  export_functions <- get_exported_functions("utils")
-  testthat::expect_type(export_functions, "character")
-})
---
+++ tests/testthat/test-get_exported_functions.R
@@ -0,0 +1,12 @@
+testthat::test_that("export type is right", {
+
+  export_functions <- get_exported_functions(pkg = "base")
+  testthat::expect_type(export_functions, "character")
+})
+
+
+testthat::test_that("export is correct", {
+
+  export_functions <- get_exported_functions(pkg = "base")
+  testthat::expect_equal(export_functions[1:5], c("!", "$", "&", "(", "*"))
+})
