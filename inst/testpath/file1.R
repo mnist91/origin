c263eb2a14595333387ba3e9a98343a0c1d4db29
mnist_91 <matthias.nistler@statworx.com>
2021-04-08 09:41:56 +0100

    TESTS: testing script - skip ci

--- inst/testpath/file1.R
+++ inst/testpath/file1.R
@@ -1,1 +1,6 @@
+devtools::load_all()
 mutate(iris, x = 3)
+
+originize_selection(pkgs = "dplyr", overwrite = TRUE)
+originize_file("inst/testpath/file1.R", pkgs = "dplyr", verbose = TRUE, ask_before_applying_changes = TRUE, use_markers = F)
+originize_dir("inst/testpath", pkgs = "dplyr", verbose = TRUE, ask_before_applying_changes = TRUE)
