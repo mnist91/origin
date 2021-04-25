291d7adb2b926baaf5b3e2db9c956a688da49456
mnist_91 <matthias.nistler@statworx.com>
2021-04-25 22:02:59 +0200

    BF: missing argument

--- R/originize_dir.R
+++ R/originize_dir.R
@@ -83,7 +83,7 @@ originize_dir <-
     scripts <- lapply(files, readLines)
 
     # check for empty scripts
-    empty_scripts <- vapply(scripts, length) == 0
+    empty_scripts <- vapply(X = scripts, FUN = length, FUN.VALUE = integer(1)) == 0
 
     if (all(empty_scripts)) {
       message("All provided scripts are empty")
