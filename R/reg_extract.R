769f16b6485b48e05ca256f3878a5bb03ed8590c
mnist_91 <matthias.nistler@statworx.com>
2021-06-22 23:23:26 +0200

    DOCU: link to base R function

--- R/reg_extract.R
+++ R/reg_extract.R
@@ -3,7 +3,7 @@
 #' @param x a character vector
 #' @param pattern a regular expression
 #' @param invert  logical: if TRUE, extract or replace the non-matched substrings.
-#' @param ... arguments passed to gregexpr
+#' @param ... arguments passed to \link[base]{gregexpr}
 #'
 #' @return a character vector
 reg_extract <- function(x, pattern, invert = FALSE, ...) {
