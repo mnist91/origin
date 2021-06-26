2acef0dc5a870228177fa52fa54c86d264d48520
mnist_91 <matthias.nistler@statworx.com>
2021-06-26 08:50:39 +0200

    NEW: originize_pkg

---
+++ R/get_pkgs_from_description.R
@@ -0,0 +1,70 @@
+
+#' Get Packages from the DESCRIPTION file
+#'
+#' @return character vector of package names
+#' @export
+#'
+#' @examples
+#' \notrun{
+#' get_pkgs_from_description()
+#' }
+get_pkgs_from_description <- function() {
+
+  # throws an error if no DESCription file is present
+  desc_path <- file.path(rprojroot::find_package_root_file(), "DESCRIPTION")
+
+  desc <- readLines(desc_path)
+
+  # Keywords with package names listed afterwards
+  triggers <- grep("Imports|Suggests|Depends", desc)
+
+  if (length(triggers) == 0) {
+    stop("No Imports, Suggests, or Depends in Description")
+  }
+
+  # arguments have indentions
+  arguments <- grep("^[[:blank:]]+", desc)
+
+  nrow_desc <- length(desc)
+  # lines with indention right after the keywords
+  is_pkg_line <- unlist(lapply(triggers[(triggers + 1) %in% arguments],
+                               function(x) {
+
+                                 # sequence of numbers starting after the trigger
+                                 x_seq <- (x + 1):nrow_desc
+
+                                 # which parts of this sequence are arguments
+                                 match_seq <- x_seq %in% arguments
+
+                                 # cut sequence as soon as the first non-argument
+                                 # appears in the sequence
+                                 out <- x_seq[cumsum(match_seq) == seq_along(match_seq)]
+                                 return(out)
+                               }))
+
+  if (length(is_pkg_line) == 0) {
+    warning("No Packages found in the DESCRIPTION")
+    return(NULL)
+  }
+
+  pkg_lines <- desc[is_pkg_line]
+
+  # extract the package name
+  pkgs <- regmatches(x = pkg_lines,
+                     m = regexpr(pattern = "[A-z\\.0-9]+",
+                                 text = pkg_lines),
+                     invert = FALSE)
+  # exlcude R since it can appear in Depends but is not a package
+  pkgs <- pkgs[pkgs != "R"]
+
+  if (length(pkgs) == 0) {
+    warning("No Packages found in the DESCRIPTION")
+    return(NULL)
+  }
+
+  pkgs <- unique(pkgs)
+
+  return(pkgs)
+}
+
+
---
+++ R/originize_pkg.R
@@ -0,0 +1,129 @@
+#' Originize a Package Project
+#'
+#' @description It shares the functionality of originize_dir but is designed to
+#'  be used within R-package projects.
+#'
+#' @param path path to the package project root by \link[rprojroot]{find_package_root_file}
+#' @param pkgs a character vector of package names, defaults to packages mentioned
+#'  in the DESCRIPTION file if the option `origin.pkgs` is not set.
+#' @param recursive logical. Should the listing recurse into directories?
+#'  See \link[base]{list.files}
+#' @param files_pattern A regular expression. Only file names which match the
+#'  regular expression will be returned. See \link[base]{list.files}
+#' @param ignore_case logical. Should pattern-matching be case-insensitive?
+#'  See \link[base]{list.files}
+#' @param exclude_files list of files to be excluded
+#' @template overwrite
+#' @template ask_before_applying_changes
+#' @template ignore_comments
+#' @template check_conflicts
+#' @template check_base_conflicts
+#' @template add_base_packages
+#' @template excluded_functions
+#' @template verbose
+#' @template use_markers
+#' @template check_local_conflicts
+#'
+#' @return NULL
+#' @export
+#'
+originize_pkg <-
+  function(path = rprojroot::find_package_root_file(),
+           pkgs = getOption("origin.pkgs", get_pkgs_from_description()),
+           recursive = TRUE,
+           files_pattern = "\\.R$",
+           ignore_case = TRUE,
+           exclude_files = NULL,
+           overwrite = getOption("origin.overwrite", TRUE),
+           ask_before_applying_changes =
+             getOption("origin.ask_before_applying_changes", TRUE),
+           ignore_comments = getOption("origin.ignore_comments", TRUE),
+           check_conflicts = getOption("origin.check_conflicts", TRUE),
+           check_base_conflicts = getOption("origin.check_base_conflicts", TRUE),
+           add_base_packages = getOption("origin.add_base_packages", FALSE),
+           excluded_functions = getOption("origin.excluded_functions", list()),
+           verbose = getOption("origin.verbose", FALSE),
+           use_markers = getOption("origin.use_markers_for_logging", TRUE),
+           check_local_conflicts = getOption("origin.check_local_conflicts", TRUE)
+  ) {
+
+    if (!check_base_conflicts && add_base_packages) {
+      stop("When adding base packages checking for ",
+           "potential conflicts is required!")
+    }
+
+    files <- list_files(path = path,
+                        exclude_folders = c("renv", "packrat"),
+                        full.names = TRUE,
+                        include.dirs = FALSE,
+                        recursive = recursive,
+                        pattern = files_pattern,
+                        ignore.case = ignore_case)
+
+
+    # TODO: non absolute paths
+    if (!is.null(exclude_files)) {
+      if (any(!exclude_files %in% files)) {
+        stop("File to exclude not in given path\n",
+             exclude_files[!exclude_files %in% files])
+      }
+      files <- files[!files %in% exclude_files]
+    }
+
+
+    # warning if many files are about to be originized
+    n_files <- length(files)
+    if (n_files > 20) {
+      cat(sprintf("You are about to originize %s files.\nProceed?", n_files))
+      if (interactive()) {
+        answer <- menu(choices = c("YES", "NO", "Show files")) # nocov
+      } else {
+        answer <- 1
+      }
+
+      if (answer == 2) {
+        stop("Execution halted") # nocov
+      } else if (answer == 3) {
+        print(files)
+        cat("\nProceed?")
+        answer2 <- menu(choices = c("YES", "NO")) # nocov
+
+        if (answer2 == 2) {
+          stop("Execution halted") # nocov
+        }
+      }
+    }
+
+    # read file
+    scripts <- suppressWarnings(lapply(files, readLines))
+
+    # check for empty scripts
+    empty_scripts <- vapply(X = scripts, FUN = length, FUN.VALUE = integer(1)) == 0
+
+    if (all(empty_scripts)) {
+      message("All provided scripts are empty")
+      return(invisible(NULL))
+    } else if (any(empty_scripts)) {
+      scripts <- scripts[!empty_scripts]
+      files <- files[!empty_scripts]
+    }
+
+    originize_wrap(scripts = scripts,
+                   files = files,
+                   type = "writeLines",
+                   pkgs = pkgs,
+                   overwrite = overwrite,
+                   ask_before_applying_changes = ask_before_applying_changes,
+                   ignore_comments = ignore_comments,
+                   check_conflicts = check_conflicts,
+                   check_base_conflicts = check_base_conflicts,
+                   add_base_packages = add_base_packages,
+                   excluded_functions = excluded_functions,
+                   verbose = verbose,
+                   use_markers = use_markers,
+                   check_local_conflicts = check_local_conflicts)
+
+    return(invisible(NULL))
+
+  }
+
