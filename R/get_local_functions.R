6eb82d5e94ca381f5be6d06f449c157d0c062eb9
mnist_91 <matthias.nistler@statworx.com>
2021-08-15 14:29:53 +0200

    IMP: include timeout limit when looking for local functions

--- R/get_local_functions.R
+++ R/get_local_functions.R
@@ -1,7 +1,7 @@
-#' Find all user defined functions in the project
+#' Find All User Defined functions in the Project
 #'
 #' @param path Path in which all defined function names should be
-#'  found and retrieved.
+#'  found and retrieved. Defaults to the current working directory.
 #'
 #' @return character vector of function names
 #' @export
@@ -19,15 +19,37 @@ get_local_functions <- function(path = ".") {
     return(character(0))
   }
 
-  # all R files that do not live in the foldrs renv/packrat/tests
-  files <- list_files(path = root,
-                      exclude_folders = c("renv", "packrat", "tests"),
-                      full.names = TRUE,
-                      include.dirs = FALSE,
-                      recursive = TRUE,
-                      pattern = "\\.R$",
-                      ignore.case = TRUE)
 
+
+  # In case that the project folder is very deep, for example because a huge
+  # share file sevrver is linked in the project folder, a timeout is triggered
+  time_limit <- 180
+  setTimeLimit(cpu = time_limit, elapsed = time_limit, transient = TRUE)
+  on.exit({
+    setTimeLimit(cpu = Inf, elapsed = Inf, transient = FALSE)
+  })
+
+  tryCatch({
+    # all R files that do not live in the foldrs renv/packrat/tests
+    files <- list_files(path = root,
+                        exclude_folders = c("renv", "packrat", "tests"),
+                        full.names = TRUE,
+                        include.dirs = FALSE,
+                        recursive = TRUE,
+                        pattern = "\\.R$",
+                        ignore.case = TRUE)
+
+  }, error = function(e) {
+    if (grepl("time limit", e$message)) {
+      stop(paste(e$message,
+                 "when searching all files for function definitions.",
+                 "Consider setting",
+                 "options(origin.check_local_conflicts = FALSE)."))
+    } else {
+      # error not related to timeout
+      stop(e)
+    }
+  })
 
 
   # read files
